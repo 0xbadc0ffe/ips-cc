8344ba469d0f4f84d26938d98ea47e41ba2e5101
Stefano 0x7cc <0x07cc@users.noreply.github.com>
2020-06-09 22:50:44 +0000

    Aggiunto controllo versione IP e Lunghezza Header IPv4

--- main.py
+++ main.py
@@ -1,23 +1,51 @@
 #!/usr/bin/env python3
 from netfilterqueue import NetfilterQueue
 
+numero_queue = 33
+
 def print_and_accept(pkt):
 	print('-------------')
 	payload = pkt.get_payload()
-	#Fino al 20 sicuro e' la parte di IPv4
-	portaSource = payload[20:22].hex()
+	payload_hex = payload.hex()
+	
+	# Verifica della versione di IP del pacchetto:
+	# Se non e' 4, non effettuo controlli
+	# e lo accetto.
+	versioneIP = payload_hex[0]
+	if versioneIP != '4':
+		print("Pacchetto non IPv4")
+		pkt.accept()
+		return
+
+	inizioTCP = calcola_lunghezza_ipv4(payload_hex[1])
+
+	portaSource = payload[inizioTCP:inizioTCP+2].hex()
 	portaSourceint = int(portaSource, 16)
 	print("porta source: " + str(portaSourceint))
-	portaDest = payload[22:24].hex()
+	
+	portaDest = payload[inizioTCP+2:inizioTCP+4].hex()
 	portaDestint = int(portaDest, 16)
 	print("porta destinazione: " + str(portaDestint))
+	
+	# TODO: verificare se SYN e' settato, in tal caso -> accept()
 	print(pkt)
-	print(payload.hex())
+	print(payload_hex)
 	print('-------------')
 	pkt.accept()
 
+# Funzione che calcola la lunghezza del pacchetto IPv4
+# basandosi sul valore IHL (Internet Header Length).
+#
+# Documentazione di riferimento:
+# https://en.wikipedia.org/wiki/IPv4#IHL
+def calcola_lunghezza_ipv4(carattere):
+	lunghezza = 20
+	ihl = int(carattere, 16)
+	lunghezza = (ihl*32)//8
+	return lunghezza
+
 nfqueue = NetfilterQueue()
-nfqueue.bind(33, print_and_accept)
+nfqueue.bind(numero_queue, print_and_accept)
 
 try:
 	nfqueue.run()
